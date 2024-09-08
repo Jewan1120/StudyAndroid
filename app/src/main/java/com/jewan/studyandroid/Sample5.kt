bb43649a0399ac5ffc1ddd35475202e1e4a92250
Jewan1120 <hawaii1468@gmail.com>
2024-09-08 21:27:42 +0900

    feat: 코틀린 Data Class 문법 - 자바의 POJO

---
+++ app/src/main/java/com/jewan/studyandroid/Sample5.kt
@@ -0,0 +1,17 @@
+package com.jewan.studyandroid
+
+// POJO : Plain Old Java Object
+// 자바는 다 작성해야 함
+// Data Class : 코틀린의 POJO..?
+data class Student(val name: String, val age: Int, val birthDay: String, var friend: String)
+// 이거 하나로 toString, hashCode, equals, copy 다 만들어짐
+
+class Teacher(val name: String, val age: Int, val birthDay: String, var friend: String)
+
+fun main() {
+    val jewan = Student("Jewan", 5, "1120", "Taekbae")
+    val taekbae = Teacher("Taekbae", 2, "0815", "Jewan")
+
+    println(jewan) //    Student(name=Jewan, age=5, birthDay=1120, friend=Taekbae) -> toString
+    println(taekbae) //    com.jewan.studyandroid.Teacher@7291c18f
+}
