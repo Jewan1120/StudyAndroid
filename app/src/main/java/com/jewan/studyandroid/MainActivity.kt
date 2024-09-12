89867133d1ddd0b4fba5d5ac4c05ba97b4a7474c
Jewan1120 <hawaii1468@gmail.com>
2024-09-13 01:07:55 +0900

    feat: remember와 state를 이용해서 단위 변환기 구현

--- app/src/main/java/com/jewan/studyandroid/MainActivity.kt
+++ app/src/main/java/com/jewan/studyandroid/MainActivity.kt
@@ -24,12 +24,17 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jewan.studyandroid.ui.theme.StudyAndroidTheme
+import kotlin.math.roundToInt
 
 class MainActivity :
     ComponentActivity() { // 안드로이드의 시작점, Activity는 화면이라고 생각하면 됨 -> 사용자 인터페이스를 가진 단일 화면
@@ -89,6 +94,22 @@ fun UnitConverterPreview() {
 
 @Composable
 fun UnitConverter() {
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("0.00") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val iconversionFactor = remember { mutableStateOf(0.01) }
+    val oconversionFactor = remember { mutableStateOf(0.01) }
+
+    fun convertUnits() {
+        // ?: elvis operator
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0 // null일 경우 초기화
+        val result = (inputValueDouble * iconversionFactor.value / oconversionFactor.value * 100.0).roundToInt() / 100.0
+        outputValue = result.toString()
+    }
+
     Column(
         // modifier는 Column의 소괄호 안에 들어가야 함
         modifier = Modifier.fillMaxSize(), // 전체 화면으로 채움
@@ -97,35 +118,72 @@ fun UnitConverter() {
     ) {
         Text("Unit Converter") // 여기에 Padding을 추가해서 칸을 띄울 수도 있음
         Spacer(modifier = Modifier.height(16.dp)) // 기기 화면 픽셀 밀도에 따른 상대적인 픽셀값
-        OutlinedTextField(value = "", onValueChange = {
-
-        })
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = {
+                inputValue = it // 필드에 적힌 값을 inputValue에 넣어줌
+                convertUnits()  // 값이 적힐 때마다 계산
+            },
+            label = { Text("Enter Value") }, // label은 그냥 표시
+            placeholder = { Text("Do it") }) // placeholder는 커서로 눌렀을 때 표시
         Row {
             Box {
-                Button(onClick = {}) {
-                    Text("Select")
+                Button(onClick = { iExpanded = true }) { // 드롭다운 표시
+                    Text(inputUnit)
                     Icon(Icons.Default.ArrowDropDown, "")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = {}) {
-                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Meters") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Kilometers") }, onClick = {})
+                DropdownMenu(
+                    expanded = iExpanded,
+                    onDismissRequest = { iExpanded = false }) { // 취소되면 닫기
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Centimeters"
+                        iconversionFactor.value = 0.01
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Meters"
+                        iconversionFactor.value = 1.0
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = { Text("Kilometers") }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Kilometers"
+                        iconversionFactor.value = 1000.0
+                        convertUnits()
+                    })
                 }
             }
             Spacer(modifier = Modifier.width(16.dp))
             Box {
-                Button(onClick = {}) {
-                    Text("Select")
+                Button(onClick = { oExpanded = true }) { // 드롭다운 표시
+                    Text(outputUnit)
                     Icon(Icons.Default.ArrowDropDown, "")
                 }
-                DropdownMenu(expanded = true, onDismissRequest = {}) {
-                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Meters") }, onClick = {})
-                    DropdownMenuItem(text = { Text("Kilometers") }, onClick = {})
+                DropdownMenu(expanded = oExpanded, onDismissRequest = { oExpanded = false }) {
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Centimeters"
+                        oconversionFactor.value = 0.01
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Meters"
+                        oconversionFactor.value = 1.00
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = { Text("Kilometers") }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Kilometers"
+                        oconversionFactor.value = 1.000
+                        convertUnits()
+                    })
                 }
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Text("Result : ")
+        Text("Result : $outputValue ${outputUnit}")
     }
 }
