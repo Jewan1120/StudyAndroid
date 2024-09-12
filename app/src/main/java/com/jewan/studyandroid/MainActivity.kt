9b560359dec6e5775ee760c915a0a73ebc3e9350
Jewan1120 <hawaii1468@gmail.com>
2024-09-12 22:38:25 +0900

    feat: Button 생성 및 메세지 출력 - OutlinedTextField : 텍스트를 입력할 수 있는 입력 필드 컴포넌트

--- app/src/main/java/com/jewan/studyandroid/MainActivity.kt
+++ app/src/main/java/com/jewan/studyandroid/MainActivity.kt
@@ -1,30 +1,29 @@
 package com.jewan.studyandroid
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.jewan.studyandroid.ui.theme.StudyAndroidTheme
 
-class MainActivity : ComponentActivity() { // 안드로이드의 시작점, Activity는 화면이라고 생각하면 됨 -> 사용자 인터페이스를 가진 단일 화면
+class MainActivity :
+    ComponentActivity() { // 안드로이드의 시작점, Activity는 화면이라고 생각하면 됨 -> 사용자 인터페이스를 가진 단일 화면
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             StudyAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Jewan",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                UnitConverter()
             }
         }
     }
@@ -44,4 +43,28 @@ fun GreetingPreview() {
     StudyAndroidTheme {
         Greeting("Jewan")
     }
+}
+
+@Composable
+fun UnitConverter() {
+    Column {
+        // 이 부분에 UI요소들을 하나 씩 넣음
+        Text("Unit Converter")
+        OutlinedTextField(value = "Enter Value", onValueChange = {
+            // 실행되어야 할 로직을 입력
+        })
+        Row {
+            val context = LocalContext.current
+            Button(onClick = { Toast.makeText(context, "ClickClick", Toast.LENGTH_LONG).show() }) {
+                Text("Click")
+            }
+        }
+        Text("Result : ")
+    }
+}
+
+@Preview
+@Composable
+fun UnitConverterPreview() {
+    UnitConverter()
 }
