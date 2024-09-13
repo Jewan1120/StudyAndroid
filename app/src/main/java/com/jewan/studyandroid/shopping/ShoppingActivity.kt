affaa7b5a68c415d3f3f05c5e4eb1728b7a03d25
Jewan1120 <hawaii1468@gmail.com>
2024-09-13 17:56:34 +0900

    feat: AlertDialog를 이용해 입력받기 - 78Line에서 +연산을 사용하는 이유는 리스트를 재 할당하여 Recomposition 명확하게 일어나게 하기 위함

--- app/src/main/java/com/jewan/studyandroid/shopping/ShoppingActivity.kt
+++ app/src/main/java/com/jewan/studyandroid/shopping/ShoppingActivity.kt
@@ -1,19 +1,20 @@
 package com.jewan.studyandroid.shopping
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import android.app.AlertDialog
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,44 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.jewan.studyandroid.ui.theme.StudyAndroidTheme
 
-class MainActivity :
-    ComponentActivity() { // 안드로이드의 시작점, Activity는 화면이라고 생각하면 됨 -> 사용자 인터페이스를 가진 단일 화면
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            StudyAndroidTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Button(
-                            onClick = {},
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                            Text("물품 추가하기")
-                        }
-                        // 항목이 추가되거나 삭제되면 자동으로 업데이트
-                        LazyColumn(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(16.dp)
-                        ) {
-                            items(sItems) { }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 
 data class ShoppingItem( // 데이터 객체
     val id: Int,
@@ -66,3 +30,80 @@ data class ShoppingItem( // 데이터 객체
     var quantitiy: Int,
     var isEditing: Boolean = false
 )
+
+@Composable
+fun ShoppingListApp() {
+    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { showDialog = true },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("물품 추가하기")
+        }
+        // 항목이 추가되거나 삭제되면 자동으로 업데이트
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(sItems) { }
+        }
+    }
+    if (showDialog) {
+        AlertDialog(onDismissRequest = { showDialog = false },
+            confirmButton = { // 확인 버튼 부분을 오버라이드
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(onClick = {
+                        // 비어있지 않다면
+                        if (itemName.isNotBlank()) {
+                            val newItem = ShoppingItem(
+                                id = sItems.size + 1,
+                                name = itemName,
+                                quantitiy = itemQuantity.toInt()
+                            )
+                            // 왜 + 연산??
+                            // 새로운 리스트를 할당함으로써 컴포저블 재구성(recomposition)이 더 명확하게 발생
+                            sItems += newItem
+                            showDialog = false
+                            itemName = ""
+                        }
+                    }) { Text("추가") }
+                    Button(onClick = { showDialog = false }) { Text("취소") }
+                }
+
+            },
+            title = { Text("상품 추가") },
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = { itemName = it },
+                        singleLine = true, // 다중 줄을 허용하지 않음
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = { itemQuantity = it },
+                        singleLine = true, // 다중 줄을 허용하지 않음
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
+            })
+    }
+}
