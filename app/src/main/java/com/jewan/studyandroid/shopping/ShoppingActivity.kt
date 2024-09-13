a23ae04c4aee610822c5f7550df1685b8b965d39
Jewan1120 <hawaii1468@gmail.com>
2024-09-13 19:01:04 +0900

    feat: LazyColumn에 들어갈 Composable구현

--- app/src/main/java/com/jewan/studyandroid/shopping/ShoppingActivity.kt
+++ app/src/main/java/com/jewan/studyandroid/shopping/ShoppingActivity.kt
@@ -1,17 +1,30 @@
 package com.jewan.studyandroid.shopping
 
 import android.app.AlertDialog
+import android.inputmethodservice.Keyboard.Row
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 
@@ -53,7 +67,9 @@ fun ShoppingListApp() {
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            items(sItems) { }
+            items(sItems) {
+                ShoppingListItem(it, {}, {})
+            }
         }
     }
     if (showDialog) {
@@ -105,5 +121,67 @@ fun ShoppingListApp() {
                     )
                 }
             })
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,    // 수정을 눌렀을 때 사용할 람다
+    onDeleteClick: () -> Unit  // 삭제를 눌렀을 때 사용할 람다
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(
+                border = BorderStroke(2.dp, Color.Gray),
+                shape = RoundedCornerShape(20) // 모서리 둥근 정도
+            )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "수량: ${item.quantitiy}", modifier = Modifier.padding(8.dp))
+        Row(Modifier.padding(8.dp)) {
+            IconButton(onClick = onEditClick) { // 아이콘 버튼을 만들 수도 있답니다.
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
+    }
+}
+
+@Composable
+fun ShoppingItemEditor(item: ShoppingItem, onEditComplete: (String, Int) -> Unit) {
+    var editedName by remember { mutableStateOf(item.name) }
+    var editedQuality by remember { mutableStateOf(item.quantitiy.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly // 서로 거리가 동일함
+    ) {
+        Column {
+            BasicTextField(
+                value = editedName,
+                onValueChange = {editedName = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp) // 필요한 항목의 크기만 차지
+            )
+            BasicTextField(
+                value = editedQuality,
+                onValueChange = {editedQuality = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp) // 필요한 항목의 크기만 차지
+            )
+        }
+        Button(onClick = {
+            isEditing = false
+            onEditComplete(editedName, editedQuality.toIntOrNull() ?: 1)
+        }) { Text("저장") }
     }
 }
